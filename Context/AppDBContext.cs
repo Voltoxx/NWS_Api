03f425f1a3c4c39589a5f99681ed83379f9c99d0
Nicolas VAREILLE <nicolas.vareille@epitech.eu>
2023-06-02 10:14:59 +0200

    Fix dbcontext issue

--- Context/AppDBContext.cs
+++ Context/AppDBContext.cs
@@ -1,4 +1,4 @@
-﻿using Microsoft.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore;
 using NWS_Api1.Models;
 
 
@@ -6,6 +6,13 @@ namespace NWS_Api1.Context
 {
     public class AppDBContext : DbContext
     {
+        // Ta Classe n'avait pas de constructeur par défaut. Le problème venait d'ici.
+        // Un constructeur par défaut est un constructeur qui ne prend aucun argument.
+        // Pour vérifier si une classe en possède un, tu as simplement à faire:
+        // new TaClasse();
+        // Si le compilateur te sors une erreur, c'est que tu n'en as pas :)
+        public AppDBContext() { }
+
         public AppDBContext(DbContextOptions<AppDBContext> options) : base(options)
         {
 
