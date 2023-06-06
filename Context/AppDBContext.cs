1f0ec55c370e8b696f7711e63ce6a7ca699f1a38
Arthur <arthur.philippe21@gmail.com>
2023-06-06 09:05:56 +0200

    Ajoutez des fichiers projet.

---
+++ Context/AppDBContext.cs
@@ -0,0 +1,31 @@
+using Microsoft.EntityFrameworkCore;
+using NWS_Api1.Models;
+
+
+
+namespace NWS_Api1.Context
+{
+    public class AppDBContext : DbContext
+    {
+        // Ta Classe n'avait pas de constructeur par défaut. Le problème venait d'ici.
+        // Un constructeur par défaut est un constructeur qui ne prend aucun argument.
+        // Pour vérifier si une classe en possède un, tu as simplement à faire:
+        // new TaClasse();
+        // Si le compilateur te sors une erreur, c'est que tu n'en as pas :)
+        public AppDBContext() { }
+
+        public AppDBContext(DbContextOptions<AppDBContext> options) : base(options)
+        {
+
+        }
+
+        protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
+        {
+            string str = "Server = localhost; Port = 3306; user = root;password = root; Database = NwsEffectif";
+            base.OnConfiguring(optionsBuilder);
+            optionsBuilder.UseMySql(str, ServerVersion.AutoDetect(str));
+        }
+
+        public DbSet<Person> person { get; set; }
+    }
+}
---
+++ Controllers/PersonController.cs
@@ -0,0 +1,87 @@
+﻿using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
+using NWS_Api1.Context;
+using NWS_Api1.Models;
+
+namespace NWS_Api1.Controllers
+{
+    [ApiController]
+    [Route("api/[controller]")]
+    public class PersonController : Controller
+    {
+        
+        private readonly AppDBContext context;
+
+        public PersonController(AppDBContext context)
+        {
+            this.context = context;
+        }
+
+        
+        //GET Person/GetAllPeople
+        [HttpGet("GetAllPeople")]
+        public IEnumerable<Person> GetAllPeople()
+        {
+            return context.person;
+        }
+
+        
+        //GET Person/GetOnePerson
+        [HttpGet("GetOne/{nom}{prenom}")]
+        public IEnumerable<Person> GetOnePerson(string nom, string prenom)
+        {
+            return context.person.Where(x => x.Name == nom && x.Surname == prenom);
+           
+        }
+
+        //POST Person/InsertOnePerson
+        [HttpPost("InsertOnePerson")]
+
+        public ActionResult<Person> InsertOnePerson(Person person)
+        {
+            context.person.Add(person);
+            context.SaveChanges();
+            return Ok();
+        }
+
+        //PUT Person/UpdateOnePerson
+        [HttpPut("UpdateOnePerson")]
+
+        public ActionResult<Person> UpdateOnePerson(Person person)
+        {
+            context.Entry(person).State = EntityState.Modified;
+            context.SaveChanges();
+            return person;
+        }
+
+        //DELETE Person/DeleteOnePerson
+        [HttpDelete("DeleteOnePerson")]
+
+        public ActionResult<Person> DeleteOnePerson(Person person)
+        {
+            context.person.Remove(person);
+            context.SaveChanges();
+            return Ok();
+        }
+
+        //GET Person/AgeAscending
+        [HttpGet("AgeAscending")]
+
+        public IEnumerable<Person> AgeAscending()
+        {
+            return context.person.OrderBy(person => person.Age);
+        }
+
+        //GET Person/AlphabeticalOrder
+        [HttpGet("AlphabeticalOrder")]
+
+        public IEnumerable<string> AlphabeticOrder()
+        {
+            return context.person
+                .OrderBy(person => person.Name)
+                .ThenBy(person => person.Surname)
+                .Select(person => $"{person.Name} {person.Surname}");
+        } 
+    }
+}
